fbb6dee76beedaa3aa064e0494c500e46f3fa506
Austin <wasuen@gmail.com>
2019-09-17 16:29:28 -0700

    add value key in model

--- models/data.js
+++ models/data.js
@@ -8,7 +8,8 @@ const dataSchema = new mongoose.Schema({
   change: String,
   notes: String,
   dataStatus: String,
-  group: String
+  group: String,
+  value: String,
 })
 
 const Data = mongoose.model('Data', dataSchema)
